6a171373d4275f163456eae2d42398c43ba92c9a
Your <your@example.com>
2019-04-08 14:32:22 +0200

    Takie cos

--- app/src/main/java/lab/android_lab/MainActivity.java
+++ app/src/main/java/lab/android_lab/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //dodam sobie jakiś komentarz tak o bo mi sie nudzi :D
     }
 }
